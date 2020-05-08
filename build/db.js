6712d0735cab44643817e27e38de5152c0aad22b
Bryce Mercines <nplixel@gmail.com>
2020-05-09 04:08:54 +0800

    removed build folder

--- build/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
-const db = pool;
-exports.default = db;
